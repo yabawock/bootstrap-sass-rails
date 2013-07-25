c705fe869701d313cfbabc5edffa44fde07e7e3f
A.S. Lomoff <lomov.as@gmail.com>
2013-07-25 23:33:36 +0300

    add twitter bootstrap breadcrumbs support

--- lib/bootstrap/sass/rails/engine.rb
+++ lib/bootstrap/sass/rails/engine.rb
@@ -1,8 +1,14 @@
+require File.dirname(__FILE__) + '/../../../twitter/bootstrap/twitter-bootstrap-breadcrumbs.rb'
+
+
 module Bootstrap
   module Sass
     module Rails
       class Engine < ::Rails::Engine
         # Make rails look at the vendored assets
+        initializer 'twitter-bootstrap-rails.setup_helpers' do |app|
+          ActionController::Base.send :include, Twitter::Bootstrap::BreadCrumbs
+        end
       end
     end
   end
